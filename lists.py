657ac4a0de7b179c0cb4c577985ab43a8795dd18
Dickson Wachira <programmingwithdickson@gmail.com>
2024-06-10 12:44:26 +0300

    Remove duplicates from the list

--- lists.py
+++ lists.py
@@ -1,4 +1,7 @@
-numbers = [5,3,6,2,7,8,7]
+numbers = [5, 3, 6, 2, 7, 8, 3, 5, 4]
+uniques = []
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+print(uniques)        
 
-numbers.insert(3,16)
-print(numbers)
