8debc6ee5e699c2e48c7dc580af0a48bb9499bd0
Dickson Wachira <programmingwithdickson@gmail.com>
2024-06-10 12:30:58 +0300

    Add more list methods

--- lists.py
+++ lists.py
@@ -1,8 +1,4 @@
-matrix = [
-    [1,2,3],
-    [4,5,6,],
-    [7,8,9]
-    ]
-for rows in matrix:
-    for items in rows:
-        print(items)
+numbers = [5,3,6,2,7,8,7]
+
+numbers.insert(3,16)
+print(numbers)
