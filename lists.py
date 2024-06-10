6bcfe5ae1484fb68d324d80e8edf416c40a78c88
Dickson Wachira <programmingwithdickson@gmail.com>
2024-06-10 12:17:59 +0300

    Add 2D lists to the code

--- lists.py
+++ lists.py
@@ -1,6 +1,8 @@
-numbers = [10, 19, 28, 3, 7, 12]
-max = numbers[0]
-for number in numbers:
-    if number > max:
-        max = number
-print(max)        
+matrix = [
+    [1,2,3],
+    [4,5,6,],
+    [7,8,9]
+    ]
+for rows in matrix:
+    for items in rows:
+        print(items)
