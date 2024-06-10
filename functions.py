bdcb499775a1dfaf852d6fd858783d5006638eeb
Dickson Wachira <programmingwithdickson@gmail.com>
2024-06-10 18:57:31 +0300

    Add parameters to the function

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
-def greet_user():
-    print("Hello there!")
+def greet_user(first_name, last_name):
+    print(f"Hello {first_name} {last_name}!")
     print("Welcome aboard")
 
 
 print("Start")
-greet_user()
+greet_user("John", "Smith")
+greet_user("Mary", "Jane")
 print("Finish")    
    
